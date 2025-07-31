565ebfbe0ef4bc6b69beb526f9690b6e14ace42a
h0nt3d <akanoim@gmail.com>
2025-07-31 14:10:04 -0300

    Added Filename and Filepath

--- server.js
+++ server.js
@@ -6,33 +6,28 @@ const url = require("url");  //url
 const port = 3000;
 const fileDirectory = __dirname;
 
-
-
-
 const server = http.createServer(function(req, res) {
 	const reqURL = new URL(`http://localhost:${port}`);
+	const fileName = reqURL.pathname.replace("/", "");
+	const filePath = path.join(fileDirectory, fileName);
+
 	console.log(reqURL);
 	console.log("Working Directory: " + fileDirectory);
+	console.log("Current File: " + fileName);
 	res.write("This is a response.");
 
-	fs.readdir(fileDirectory, function(error, files) {
+	fs.readdir(fileDirectory, function(error, files) { //reading through directory
 		if (error) {
 			console.log("Error reading directory");
 		}
-		console.log("Files in directory: " + files);
+		console.log("Files in directory: ", files);
 	});
 
 	res.end();
 
 });
 
-
-
-
-
-
-
-server.listen(port, function(error) {
+server.listen(port, function(error) { //port listening
 	if (error) {
 		console.log("Something went wrong!", error);
 	}
