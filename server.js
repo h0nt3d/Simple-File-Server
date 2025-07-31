50e86da7adfbfe7268f806fe531f7bf0c5a72e48
h0nt3d <akanoim@gmail.com>
2025-07-31 19:30:19 -0300

    Added listing script in index.html

--- server.js
+++ server.js
@@ -3,21 +3,30 @@ const http = require("http");
 const path = require("path");
 
 const port = 3000;
-const fileDirectory = __dirname;
+const fileDirectory = path.join(__dirname, "files");
 
 const server = http.createServer(function(req, res) {
 	console.log("Working Directory: " + fileDirectory);
 
-	fs.readdir(fileDirectory, function(error, files) {
-		if (error) {
-			console.log("Error reading directory");
-		}
-		if (req.url="/" && req.method == "GET") {
-			console.log("Files in directory: ", files);
-			res.writeHead(200, {"Content-Type": "application/json"})
+	if (req.url == "/" && req.method == "GET") {
+		fs.readFile(path.join(__dirname, "index.html"),"utf-8", function(error, data) {
+			if (error) {
+				console.log("Error loading index.html");
+			}
+			res.writeHead(200, {"Content-Type": "text/html"});
+			res.end(data);
+		});	
+	}
+	else if (req.url == "/files" && req.method == "GET") {
+		fs.readdir(fileDirectory, function(error, files) {
+			if (error) {
+				console.log("Error reading directory");
+			}
+
+			res.writeHead(200, {"Content-Type": "application/json"});
 			res.end(JSON.stringify(files));
-		}
-	});
+		});
+	}
 });
 
 server.listen(port, function(error) {
